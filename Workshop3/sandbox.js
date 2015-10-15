b9b5b5f99cc9a499be630dcafebdc27fde59c9b9
yousgoose <yousry.elmallah@gmail.com>
2015-10-15 14:36:04 -0400

    Update sandbox.js

--- Workshop3/sandbox.js
+++ Workshop3/sandbox.js
@@ -10,31 +10,27 @@ angular.module('MyApp', [])
   self.genreSelected = self.genres[0].name;
   self.expSelected = self.explicits[0].name;
 
+  self.output = '';
 
-  self.updateSonglist = function() {
-
-  }
 
   self.submitSongs = function() {
     console.log(CONVERSIONFUNCS.converttoJson(MY_SONGS, self.expSelected));
+    self.output = CONVERSIONFUNCS.converttoJson(MY_SONGS, self.expSelected);
   }
 
   self.isExpAllowed = function() {
-
-      if (self.expSelected == 'Clean')
+    if (self.expSelected == 'Clean')
+      return false;
+    else
+    {
+      if (self.age > 20)
         return false;
       else
-      {
-        if (self.age > 20)
-          return false;
-        else
-          return true;
-      }
- 
+        return true;
+    }
   }
 
   self.checkAgeExp = function() {
-    console.log('hi');
     if(self.age < 20 || !self.age) {
       self.expSelected = self.explicits[0].name;
     }
@@ -46,98 +42,4 @@ angular.module('MyApp', [])
     }
   }
 
-  // self.Assignments = [];
-  // self.AverageGrade = null;
-  // self.latestAssignment = null;
-
-  // self.addAssignment = function(name, assignmentname, grade) {
-  //   console.log('h1');
-  //   if (name && assignmentname && grade >= 0 && grade <= 100) {
-  //     console.log('h2');
-  //     self.latestAssignment = new Assignment(name, assignmentname, grade);
-  //     // self.latestAssignment.updateAverage();
-  //     // self.AverageGrade = self.latestAssignment.getAverage();
-  //     self.Assignments.push(latestAssignment);
-  //   }
-  // }
-
-
-
 });
-
-// .controller('MyController', function (MathExpressions, MY_FOLDERS) {
-//   var self = this;
-  // self.folders = angular.copy(MY_FOLDERS);
-
-
-
-  // self.popFolder = function() {
-  //   self.folders.pop();
-  // };
-
-  // self.car = new Car('Honda', 'Accord');
-
-
-  // self.logColor = function() {
-  //   console.log(self.car.getColor())
-  // };
-
-  
-
-  // self.car = {
-  //   make: 'Honda',
-  //   model: 'Accord',
-  //   year: null,
-  //   age: null,
-  //   owners: []
-  // };
-
-  // self.addOwner = function(ownerName) {
-  //   self.car.owners.push({name: ownerName});
-  // };
-
- // self.updateYear = function(newYear) {
-  //     // if (newYear && newYear > 999) {
-  //       self.car.age = 2015 - newYear;
-  //     // } else {
-  //       // self.car.age = '';
-  //     // }
-  // };
-
-  // // $scope.$watch('ctrl.car.year', function(newVal, oldVal) {
-  // //   // console.log('New: ', newVal, ' oldVal: ', oldVal);
-  // //   if (newVal && newVal > 999) {
-  // //     self.car.age = 2015 - newVal;
-  // //   } else {
-  // //     self.car.age = '';
-  // //   }
-  // // });
-
-  // $scope.$watch(function() {
-  //   return self.car.year;
-  // } , function(newVal, oldVal) {
-
-  // });
-
-  // $scope.$watchCollection('ctrl.car.owners', function(newOwners, oldOwners) {
-  //   console.log('New: ', newOwners, ' oldVal: ', oldOwners);
-  //   if (newOwners) {
-
-  //   };
-  // });
-
-  // $scope.$watch('ctrl.person', function(newVal, oldVal) {
-  //   // console.log('New: ', newVal, ' oldVal: ', oldVal);
-  //   if (newVal) {
-
-  //   }
-  // }, true);
-
-  // self.person = {
-  //   address: {
-  //     street: '100',
-  //     apt: 5
-  //   }
-  // };
-
-// });
